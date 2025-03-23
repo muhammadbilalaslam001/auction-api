3224e813eeb1b5bdbebf86d1f0bfc341a8682eb4
muhammadbilalaslam001 <muhammadbilalaslam001@gmail.com>
2025-03-23 07:50:23 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
       : configService.get<string>('APP_FRONTEND_URL');
 
   app.enableCors({
-    origin: true,
+    origin: frontendUrl,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
